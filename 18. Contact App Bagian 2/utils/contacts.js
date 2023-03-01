34ac518503c64952e49803f391af7d32ee0e8a49
Johandika <johanelyosse@gmail.com>
2023-03-02 03:54:53 +0700

    Contact App Bagian 2 - Tambah Kontak Baru

---
+++ 18. Contact App Bagian 2/utils/contacts.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+// const chalk = require("chalk");
+// const validator = require("validator");
+
+const dirPath = "./data";
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+const dataPath = "./data/contacts.json";
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath,"[]","utf-8");
+}
+
+const loadContact = () => {
+  const file = fs.readFileSync("data/contacts.json", "utf-8");
+  const contacts = JSON.parse(file);
+  return contacts;
+};
+
+const findContact = (nama) => {
+  const contacts = loadContact();
+  const contact = contacts.find((contact)=> contact.nama.toLowerCase() === nama.toLowerCase());
+  return contact;
+}
+
+// menuliskan /menimpa data file contacts.json dengan data yang baru
+const saveContacts = (contacts) => {
+  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+}
+
+//menambahkan data contact baru
+const addContact = (contact) => {
+  const contacts = loadContact();
+  contacts.push(contact);
+  saveContacts(contacts);
+}
+
+module.exports = { loadContact, findContact, addContact};
